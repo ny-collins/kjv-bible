339ca3a0fb747007658be0570d92715166f55bf1
Collins Mwangi <mwangicollins391@gmail.com>
2025-09-24 13:45:31 +0300

    JSON plugins

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,4 +1,4 @@
-import { books } from '$lib/data/Books.json';
+import booksWithMetadata from '$lib/data/BooksWithMetadata.json';
 
 export async function GET() {
   const headers = { 'Content-Type': 'application/xml' };
@@ -15,21 +15,21 @@ export async function GET() {
   </url>`;
 
   // Add book pages
-  for (const book of books) {
+  for (const book of booksWithMetadata) {
     xml += `
   <url>
-    <loc>https://kjv-bible-7mw.pages.dev/book/${book}</loc>
+    <loc>https://kjv-bible-7mw.pages.dev/book/${book.name}</loc>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>`;
 
     // Add chapter pages
-    const bookModule = await import(`$lib/data/${book}.json`);
+    const bookModule = await import(`$lib/data/${book.name}.json`);
     const bookData = bookModule.default;
     for (const chapter of bookData.chapters) {
       xml += `
   <url>
-    <loc>https://kjv-bible-7mw.pages.dev/book/${book}/${chapter.chapter}</loc>
+    <loc>https://kjv-bible-7mw.pages.dev/book/${book.name}/${chapter.chapter}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>`;
